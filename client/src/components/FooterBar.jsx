17157ee24069528cd253d927993049a8934e6b2f
Elizabeth Lotto <elizlotto@gmail.com>
2020-11-12 18:43:11 -0500

    footer into footerbar

--- client/src/components/FooterBar.jsx
+++ client/src/components/FooterBar.jsx
@@ -1,7 +1,11 @@
 import React from 'React';
 
 const FooterBar = () => {
-  return <div>Contact information here</div>;
+  return (<footer className="footer-bar">
+    <div>Phone: (347) - 400-0311
+    </div>
+    <div>Email: john(at)johnwebberwoodwork(dot)com</div>
+  </footer>);
 };
 
 export default FooterBar;
