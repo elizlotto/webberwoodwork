ddeaf137ef5ad766193d25a39569143eac26342f
Elizabeth Lotto <elizlotto@gmail.com>
2020-11-12 19:15:40 -0500

    another update

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ const Portfolio = () => {
     /*use a loop to iterate through an array of images from the assets 
     folder to have a simple one liner to handle all the html elements.
     */
-    <div>Images will go here</div>
+    <div>Images coming soon</div>
   );
 };
 
