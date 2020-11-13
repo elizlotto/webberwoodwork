46c8a49da798a4486fdc8e2517506ace1dd3af62
Elizabeth Lotto <elizlotto@gmail.com>
2020-11-12 19:02:41 -0500

    home page filled with something

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,12 @@ const Home = () => {
   return (
     <div>
       <h1>John Webber Woodwork</h1>
-      THIS IS THE HOME
+      <section>Welcome to John Webber Woodwork! 
+      John Webber has been providing quality woodwork for Victorian Flatbush for decades. 
+      He is restoration and renovation expert for all of your detailed woodwork needs. 
+      Ranging from custom kitchen and bathroom designs to historically inspired doors and windows, 
+      John Webber can meet the needs of your landmarked Victorian home.   
+      </section>
     </div>
   );
 };
