fdc331919ba1ae28d6c4766470d3ea1cedd22742
Elizabeth Lotto <elizlotto@gmail.com>
2020-11-12 19:59:40 -0500

    nav bar fixed at top 100% width. button styling started

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './NavigationBar.css';
 
 const NavigationBar = () => {
   return (
-    <nav>
+    <nav id="navbar">
       <div class="columns">
         <div class="column"></div>
         <div class="column">
