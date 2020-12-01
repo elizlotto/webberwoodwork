46d620c03052b408244755c3c6394a9323320894
Elizabeth Lotto <elizlotto@gmail.com>
2020-11-30 20:06:10 -0500

    added logo and updated webpack for the png file

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css';
+import JwwLogo from '../assets/logo.png';
 
 const NavigationBar = () => {
   return (
@@ -8,17 +9,24 @@ const NavigationBar = () => {
       <div class="columns">
         <div class="column"></div>
         <div class="column">
-          <button><Link to="/">Home</Link></button>
-      <button><Link to="/about">About</Link></button>
-      <button><Link to="/portfolio">Gallery</Link></button>
-      <button><Link to="/testimonials">Customer Testimonials</Link></button>
-          <button><Link to="/contact">Contact Form</Link></button>
-      </div>
+          <Link to="/">
+            <img class="logo" src={JwwLogo} alt="John Webber Woodwork Logo, click for Home"></img>
+          </Link>
+          <button>
+            <Link to="/about">About</Link>
+          </button>
+          <button>
+            <Link to="/portfolio">Gallery</Link>
+          </button>
+          <button>
+            <Link to="/testimonials">Customer Testimonials</Link>
+          </button>
+          <button>
+            <Link to="/contact">Contact Form</Link>
+          </button>
+        </div>
         <div class="column"></div>
-
-  
       </div>
-     
     </nav>
   );
 };
