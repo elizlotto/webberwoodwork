93731bd9090caa2926f97d17d047462fb9791a5d
Elizabeth Lotto <elizlotto@gmail.com>
2020-11-30 22:46:07 -0500

    working on nav with bulma

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NavigationBar.css';
+//import './NavigationBar.css';
 import JwwLogo from '../assets/logo.png';
 
 const NavigationBar = () => {
   return (
-    <nav id="navbar">
+    <nav id="navbar" role="navigation" class="navbar" aria-label="main navigation">
       <div class="columns">
         <div class="column"></div>
-        <div class="column">
+        <div class="navbar-brand">
           <Link to="/">
             <img class="logo" src={JwwLogo} alt="John Webber Woodwork Logo, click for Home"></img>
           </Link>
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+          </div>
           <button>
             <Link to="/about">About</Link>
           </button>
@@ -26,7 +32,6 @@ const NavigationBar = () => {
           </button>
         </div>
         <div class="column"></div>
-      </div>
     </nav>
   );
 };
