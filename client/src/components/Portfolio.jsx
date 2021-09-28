0449c56241b9e0f3d90148b58260ad179563f857
Elizabeth Lotto <elizlotto@gmail.com>
2021-09-28 16:11:59 -0400

    Testing using webpack require.context to create an array of images this PR creates an array of images and has some psuedocode notes for how to proceed.

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './style.css';
 
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+ const images = importAll(require.context('/Users/elizabethlotto/Documents/coding/webberwoodwork/client/src/assets/hero-images', false, /\.(png|jpe?g|svg)$/));
+console.log(`images:`, images)
+//above is temp solution to determine if the function importAll is working. 
+//need to remember how to use relative path. Getting node modules error. 
+//will then need to create a func that renders the array of images. 
 const Portfolio = () => {
   return (
     /*use a loop to iterate through an array of images from the assets 
     folder to have a simple one liner to handle all the html elements.
     */
     <div className="body-div">Images coming soon</div>
+    
+    
   );
 };
 
