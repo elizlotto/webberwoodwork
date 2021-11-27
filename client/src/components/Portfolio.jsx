1fb86a65fd41a149129a6daaa6c02384f3853248
Elizabeth Lotto <elizlotto@gmail.com>
2021-11-27 08:51:35 -0500

    Add images for gallery this PR adds images for gallery

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -5,8 +5,10 @@ function importAll(r) {
   return r.keys().map(r);
 }
 
- const images = importAll(require.context('/Users/elizabethlotto/Documents/coding/webberwoodwork/client/src/assets/hero-images', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../assets/hero-images', false, /\.(png|jpe?g|svg)$/));
 console.log(`images:`, images)
+
+
 //above is temp solution to determine if the function importAll is working. 
 //need to remember how to use relative path. Getting node modules error. 
 //will then need to create a func that renders the array of images. 
