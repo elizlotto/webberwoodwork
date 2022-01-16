e8b6a20be8670f43385c055e748297d4fbf2dd82
Elizabeth Lotto <elizlotto@gmail.com>
2022-01-16 14:58:06 -0500

    update burger menu and css

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -12,61 +12,65 @@ const NavigationBar = () => {
   }
 
   return (
-    <nav id="navbar" role="navigation" className="navbar" aria-label="main navigation">
-      <div className="navbar-brand">
-        <Link to="/">
-          <img
-            className="logo"
-            src={JwwLogo}
-            width="160"
-            alt="John Webber Woodwork Logo, click for Home"
-          ></img>
-        </Link>
-        <a
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasic"
-          onClick={toggleBurgerMenu}
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-      <div id="navbarBasic" class="navbar-menu">
-        <div className="navbar-start">
-          <div className="navbar-item">
-            <Link to="/" className="btnclr" onClick={toggleBurgerMenu}>
-              <button>Home</button>
+    <section class="hero is-primary is-medium">
+      <div class="hero-head">
+        <nav id="navbar" role="navigation" className="navbar" aria-label="main navigation">
+          <div className="navbar-brand">
+            <Link to="/">
+              <img
+                className="logo"
+                src={JwwLogo}
+                width="160"
+                alt="John Webber Woodwork Logo, click for Home"
+              ></img>
             </Link>
+            <a
+              role="button"
+              className="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarMenuHeroA"
+              onClick={toggleBurgerMenu}
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
           </div>
-          <div className="navbar-item">
-            <Link to="/about" className="btnclr" onClick={toggleBurgerMenu}>
-              <button>About</button>
-            </Link>
+          <div id="navbarMenuHeroA" class="navbar-menu">
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <Link to="/" className="btnclr" onClick={toggleBurgerMenu}>
+                  <button>Home</button>
+                </Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/about" className="btnclr" onClick={toggleBurgerMenu}>
+                  <button>About</button>
+                </Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/portfolio" className="btnclr" onClick={toggleBurgerMenu}>
+                  <button>Gallery</button>
+                </Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/testimonials" className="btnclr" onClick={toggleBurgerMenu}>
+                  <button>Reviews</button>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="navbar-item">
-            <Link to="/portfolio" className="btnclr" onClick={toggleBurgerMenu}>
-              <button>Gallery</button>
-            </Link>
-          </div>
-          <div className="navbar-item">
-            <Link to="/testimonials" className="btnclr" onClick={toggleBurgerMenu}>
-              <button>Reviews</button>
-            </Link>
-          </div>
-        </div>
-      </div>
-      {/* <div className="navbar-item">
+          {/* <div className="navbar-item">
             <Link to="/contact" className="btnclr" onClick={toggleBurgerMenu}>
               <button>Contact</button>
             </Link>
           </div> */}
 
-      <div className="navbar-end"></div>
-    </nav>
+          <div className="navbar-end"></div>
+        </nav>
+      </div>
+    </section>
   );
 };
 
