424c8fca9945f3b88d0a04d906c2a8c3f12656a6
Elizabeth Lotto <elizlotto@gmail.com>
2022-01-16 23:33:42 -0500

    new layout based on client feedback

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,56 +1,25 @@
 import React from 'react';
+import Portfolio from './Portfolio';
 import './style.css';
+
 const Home = () => {
-  return (
-    <div>
-      <div className="hero-image-home"></div>
-      <div className="body-div">
-        <h1>John Webber Woodwork</h1>
-        <section>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis, dolor non congue
-          dapibus, nunc neque lobortis lorem, nec eleifend sem ligula nec sem. Cras rhoncus ipsum eu
-          nibh feugiat vestibulum. Nullam sollicitudin lacinia quam, sit amet blandit risus tempor
-          ut. Aenean congue velit ut ligula facilisis, vel imperdiet dui blandit. Aliquam
-          scelerisque sem purus, at sodales ligula elementum eu. Cras facilisis vestibulum laoreet.
-          Etiam quis tortor blandit, molestie risus nec, consequat dui. Phasellus volutpat sodales
-          bibendum. Aliquam sodales dignissim quam. Sed ac consectetur mi, ac dignissim nisi.
-          Phasellus sit amet faucibus neque, ut faucibus quam. Etiam efficitur rutrum mollis.
-          Integer et vulputate quam, quis vestibulum leo. Vivamus fermentum vulputate nunc quis
-          convallis. Aenean efficitur neque sem, at ultricies libero luctus vitae. Maecenas et ante
-          sed ligula sollicitudin volutpat vitae non neque. Integer sed sodales libero, nec
-          ullamcorper elit. Sed eget gravida nisi, in feugiat metus. Fusce non diam ac nisi
-          fermentum sodales sed tempor nunc. Fusce justo quam, dignissim in nisi interdum, porttitor
-          mattis ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non purus et
-          sem iaculis fermentum. Maecenas fermentum eget justo vel dignissim. Etiam sed diam
-          efficitur, convallis orci a, elementum neque. Suspendisse scelerisque scelerisque blandit.
-          Nam vitae mauris dictum ex pharetra commodo eget in nunc. Duis placerat elementum sapien,
-          in aliquet enim facilisis eu. Maecenas vehicula dignissim urna, nec imperdiet tortor
-          aliquam eu. Ut vitae nulla eget erat consequat vulputate eu vel urna. Nulla maximus mattis
-          mi, sagittis molestie magna congue et. Nunc varius dapibus ex, non fermentum nisl tempor
-          sed. Praesent in mollis lectus, eu bibendum magna. Nunc pellentesque congue quam sed
-          lobortis. Etiam eu lectus a dolor bibendum blandit. In vitae tempus mi. Integer quis
-          finibus augue. Phasellus velit nulla, commodo et metus eget, lobortis vestibulum enim.
-          Nunc dictum venenatis nibh eu interdum. Praesent at bibendum libero. Integer velit eros,
-          mattis in eleifend at, condimentum nec purus. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Phasellus eget ultrices dui. Nulla et nunc molestie, gravida velit vitae,
-          malesuada magna. Donec malesuada lorem eget lectus laoreet, eu fringilla tortor volutpat.
-          Vivamus pretium risus eget ultrices varius. Fusce at arcu lacinia, ornare metus a,
-          faucibus ex. Praesent ac lectus dictum, aliquam diam quis, gravida ante. Morbi fringilla
-          tristique augue, vitae tempus tortor placerat in. Quisque quis laoreet tortor. Aenean
-          vitae turpis elementum, luctus mi id, dapibus felis. Aliquam tempor purus ut velit rutrum,
-          eget tristique tellus placerat. Quisque finibus, diam ullamcorper pretium interdum, turpis
-          magna sodales nisl, sit amet commodo diam augue vitae eros. Duis velit leo, feugiat porta
-          dolor pulvinar, ornare rutrum purus. Quisque sollicitudin finibus mi in gravida.
-          Vestibulum ac lectus massa. Sed id quam eleifend, ornare tortor sit amet, viverra nulla.
-          Vivamus sed congue diam. Fusce commodo dui quis libero dignissim, sed tincidunt leo
-          pharetra. Nullam finibus maximus quam, nec vulputate lacus venenatis quis. Cras porttitor
-          commodo tortor, ac sagittis sapien sagittis non. Nullam at urna quam. Aenean eget sapien
-          quam. Curabitur tempus tortor at purus sollicitudin scelerisque. Quisque bibendum, dui eu
-          pulvinar pharetra, diam dolor pellentesque risus, a lobortis justo nisi vel elit. Nunc
-          elit augue, lacinia vitae gravida sit amet, vulputate quis eros.
-        </section>
-      </div>
-    </div>
-  );
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  const imagesObj = importAll(require.context('../assets/hero-images', true, /\.(png|jpe?g|svg)$/));
+  //the above returns an array of objects with images on .default
+
+  const createImagesArray = (img) => {
+    const valuesToArray = [];
+    img.forEach((obj) => {
+      valuesToArray.push(obj.default);
+    });
+    return valuesToArray;
+  };
+
+  const imagesArray = createImagesArray(imagesObj);
+
+  return <Portfolio imagesArray={imagesArray} />;
 };
 export default Home;
--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -4,68 +4,19 @@ import './NavigationBar.css';
 import JwwLogo from '../assets/logo.png';
 
 const NavigationBar = () => {
-  function toggleBurgerMenu() {
-    document.querySelector('.navbar-menu').classList.toggle('is-active');
-  }
-
   return (
     <section className="hero is-primary is-medium">
       <div className="hero-head">
-        <nav id="navbar" role="navigation" className="navbar" aria-label="main navigation">
-          <div className="navbar-brand">
-            <Link to="/">
-              <img
-                className="logo"
-                src={JwwLogo}
-                width="160"
-                alt="John Webber Woodwork Logo, click for Home"
-              ></img>
-            </Link>
-            <a
-              role="button"
-              className="navbar-burger burger"
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="navbarMenuHeroA"
-              onClick={toggleBurgerMenu}
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </a>
-          </div>
-          <div id="navbarMenuHeroA" className="navbar-menu">
-            <div className="navbar-end">
-              <div className="navbar-item">
-                <Link to="/" className="btnclr" onClick={toggleBurgerMenu}>
-                  <button>Home</button>
-                </Link>
-              </div>
-              <div className="navbar-item">
-                <Link to="/about" className="btnclr" onClick={toggleBurgerMenu}>
-                  <button>About</button>
-                </Link>
-              </div>
-              <div className="navbar-item">
-                <Link to="/portfolio" className="btnclr" onClick={toggleBurgerMenu}>
-                  <button>Gallery</button>
-                </Link>
-              </div>
-              <div className="navbar-item">
-                <Link to="/testimonials" className="btnclr" onClick={toggleBurgerMenu}>
-                  <button>Reviews</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-          {/* <div className="navbar-item">
-            <Link to="/contact" className="btnclr" onClick={toggleBurgerMenu}>
-              <button>Contact</button>
-            </Link>
-          </div> */}
-
-          <div className="navbar-end"></div>
-        </nav>
+        <Link to="/">
+          <img
+            className="logo"
+            src={JwwLogo}
+            width="300"
+            alt="John Webber Woodwork Logo, click for Home"
+          ></img>
+        </Link>
+        <h2>Brooklyn, New York</h2>
+        <div className="header-bottom"></div>
       </div>
     </section>
   );
